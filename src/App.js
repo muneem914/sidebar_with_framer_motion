177e8ccf8244c42b6a3c92bb5dc988ecdb13cc74
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2022-09-05 01:50:33 +0600

    packages added and structured folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
+import Messages from './pages/Messages';
+import Analytics from './pages/Analytics';
+import FileManager from './pages/FileManager';
+import Order from './pages/Order';
+import Saved from './pages/Saved';
+import Settings from './pages/Settings';
+// npm i framer-motion react-router-dom react-icons
 function App() {
   return (
-    <div className="App">
-      <h1>Hello starter</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Dashboard/>} />
+          <Route path='/users' element={<Users/>} />
+          <Route path='/messages' element={<Messages/>} />
+          <Route path='/analytics' element={<Analytics/>} />
+          <Route path='/file_manager' element={<FileManager/>} />
+          <Route path='/order' element={<Order/>} />
+          <Route path='/saved' element={<Saved/>} />
+          <Route path='/settings' element={<Settings/>} />
+          <Route path='*' element={<>Not Found</>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Analytics.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <div>Analytic</div>
+  )
+}
+
+export default Analytics
